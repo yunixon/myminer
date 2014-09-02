40350f2a0c1c0b2176e103e7ec33fbbd4150db76
Yuri Skurikhin <yunixon@gmail.com>
2014-09-02 13:01:31 +0000

    Bootstrap, Simple_form, Haml, Configs controller

--- app/controllers/configurations_controller.rb
+++ app/controllers/configurations_controller.rb
@@ -1,4 +1,56 @@
 class ConfigurationsController < ApplicationController
+  
+  before_action :set_config, only: [:show, :edit, :update]
+  layout 'main'
+  
   def index
+    @configurations = ::Configuration.all
   end
+  
+  def new
+    @configuration = ::Configuration.new
+  end
+  
+  def edit
+  end
+  
+  def create
+    @configuration = ::Configuration.new(configuration_params)
+    if @configuration.save
+      flash[:notice] = "Successfully created Config." 
+      redirect_to @configuration
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    if @configuration.update(configuration_params)
+      flash[:success] = "Config updated"
+      redirect_to @configuration
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+  end
+  
+  def destroy
+    ::Configuration.find(params[:id]).destroy
+    flash[:success] = "Config destroyed."
+    redirect_to root_url
+  end
+  
+  private
+  
+  def configuration_params
+    params.require(:configuration).permit(:nick, :cpu, :motherboard, :video,
+      :miner, :config)
+  end
+  
+  def set_config
+    @configuration = ::Configuration.find(params[:id])
+  end
+ 
 end
