5ac5c7a17c1b7d8155addb71d916327bced23016
Yuri Skurikhin <yunixon@gmail.com>
2014-09-03 13:39:48 +0000

    Change schema

--- db/migrate/20140902095136_create_configurations.rb
+++ db/migrate/20140902095136_create_configurations.rb
@@ -2,9 +2,10 @@ class CreateConfigurations < ActiveRecord::Migration
   def change
     create_table :configurations do |t|
       t.string :nick
-      t.string :cpu
-      t.string :motherboard
-      t.string :video
+      t.integer :hw_type_id
+      t.string :hw_name
+      t.string :mb
+      t.string :driver
       t.string :miner
       t.text   :config
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902095136) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "configurations", force: true do |t|
-    t.string   "nick"
-    t.string   "cpu"
-    t.string   "motherboard"
-    t.string   "video"
-    t.string   "miner"
-    t.text     "config"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
