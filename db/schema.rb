95589520983b3184b27f56ac152b2ef79a187a53
Yuri Skurikhin <yunixon@gmail.com>
2014-09-03 13:46:47 +0000

    Temporary null index for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140902095136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "configurations", force: true do |t|
+    t.string   "nick"
+    t.integer  "hw_type_id"
+    t.string   "hw_name"
+    t.string   "mb"
+    t.string   "driver"
+    t.string   "miner"
+    t.text     "config"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
